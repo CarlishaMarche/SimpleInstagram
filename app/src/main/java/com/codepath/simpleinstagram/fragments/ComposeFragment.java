032a003d85f78d2edfac5544552ecb0b6a7a1861
camsim99 <csimon@cs.hmc.edu>
2019-07-10 14:07:16 -0700

    Moved contents from Compose Activity to Compose Fragment

--- app/src/main/java/com/codepath/simpleinstagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/codepath/simpleinstagram/fragments/ComposeFragment.java
@@ -1,10 +1,111 @@
 package com.codepath.simpleinstagram.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.codepath.simpleinstagram.Post;
+import com.codepath.simpleinstagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.util.List;
 
 public class ComposeFragment extends Fragment {
 
     private final String TAG = "ComposeFragment";
+    //TODO fix
+    private static final String imagePath = "/Desktop/images.jpg";
+    private EditText descriptionInput;
+    private Button createButton;
+    private Button refreshButton;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        View.setContentView(R.layout.activity_home);
+
+        descriptionInput = view.findViewById(R.id.descriptionView);
+        createButton = view.findViewById(R.id.submitDescriptionBtn);
+        refreshButton = view.findViewById(R.id.refreshDescriptionBtn);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String description = descriptionInput.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+
+                final File file = new File(imagePath);
+                final ParseFile parseFile = new ParseFile(file);
+
+                createPost(description, parseFile, user);
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadTopPosts();
+            }
+        });
+
+    }
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e==null) {
+                    Log.d("HomeActivity", "SUCCESSSUCCESSSUCESS!");
+                }
+                else {
+                    Log.d("HomeActivity", "FFFAAIILLUUUURREEEEE!");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void loadTopPosts() {
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "Post[" + i + "] =" +
+                                objects.get(i).getDescription() +
+                                "\nusername = " + objects.get(i).getUser().getUsername());
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
 }
